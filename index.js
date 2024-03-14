33875464381afa91685d874c69e483f6c84fa17e
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2024-03-13 21:51:01 -0300

    Manipulando o DOM

---
+++ index.js
@@ -0,0 +1,10 @@
+var name = document.querySelector("#exampleInputName");
+var gender = document.querySelectorAll("#form-user-create [name=gender]:checked");
+var birth = document.querySelector("#exampleInputNameBirth");
+var country = document.querySelector("#exampleInputCountry");
+var email = document.querySelector("#exampleInputEmail");
+var password = document.querySelector("#exampleInputPassword");
+var photo = document.querySelector("#exampleInputFile");
+var admim = document.querySelector("#exampleInputAdmin");
+
+console.log(gender);
