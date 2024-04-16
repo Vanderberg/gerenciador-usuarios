084ffa6058cc28afbc8ad509540c045d02033435
Vanderberg Nunes <vanderberg.nunes@softplan.com.br>
2024-04-15 21:22:02 -0300

    Usando sessionStorage

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -7,6 +7,7 @@ class UserController {
 
         this.onSubmit();
         this.onEdit();
+        this.selectAll();
     }
 
     onEdit() {
@@ -85,6 +86,7 @@ class UserController {
             this.getPhoto(this.formCreateEl).then(
                 (content) => {
                     values.photo = content;
+                    this.insert(values);
                     this.addLine(values);
                     this.formCreateEl.reset()
                     btnSubmit.disabled = false;
@@ -155,8 +157,43 @@ class UserController {
             user.email, user.password, user.photo, user.admin);
     }
 
+    getUsersStorage() {
+
+        let users = [];
+        if (sessionStorage.getItem("users"))
+            users = JSON.parse(sessionStorage.getItem("users"));
+
+        return users;
+    }
+
+    selectAll() {
+        let users = this.getUsersStorage();
+
+        users.forEach(dataUser => {
+
+            let user = new User();
+
+            user.loadFromJSON(dataUser);
+
+            this.addLine(user)
+        });
+
+    }
+
+    insert(data) {
+
+        let users = this.getUsersStorage();
+
+        users.push(data);
+
+        sessionStorage.setItem("users", JSON.stringify(users));
+
+    }
+
     addLine(dataUser) {
         let tr = document.createElement('tr');
+
+
 
         tr.dataset.user = JSON.stringify(dataUser);
 
